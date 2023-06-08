38e62e760da3f0e1331fac73e2a0475ef21e635e
Lizzy-dev27 <chaukee555@gmail.com>
2023-06-08 07:45:49 -0700

    Hello world project

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS.H
+
+#include <stdib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton projects
+ */
+
+typedef struct listint_s
+{
+	int n:
+	struct listin_s *next;
+} listint_t;
+
+size_t print_listin(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
